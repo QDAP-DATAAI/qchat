eb245ccf5f8c4fa841c609d758d579827d0545ff
Bruno Piovan <bruno.piovan@chde.qld.gov.au>
2024-03-25 17:08:51 +1000

    fixed feedback submission on existing messages

--- src/features/chat/chat-ui/chat-message-container.tsx
+++ src/features/chat/chat-ui/chat-message-container.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from "react"
 import { useRouter } from "next/navigation"
 import ChatLoading from "@/components/chat/chat-loading"
-import ChatRow from "@/components/chat/chat-row"
+import ChatRow, { ChatRowProps } from "@/components/chat/chat-row"
 import { useSession } from "next-auth/react"
 import { useChatContext } from "./chat-context"
 import { ChatHeader } from "./chat-header"
@@ -28,23 +28,23 @@ export const ChatMessageContainer: React.FC<Props> = ({ chatThreadId }) => {
   }, [isLoading, router])
 
   return (
-    <div className="h-full overflow-y-auto bg-altBackground" ref={scrollRef}>
+    <div className="bg-altBackground h-full overflow-y-auto" ref={scrollRef}>
       <div className="flex h-auto justify-center p-4">
         <ChatHeader />
       </div>
-      <div className="flex h-auto flex-1 flex-col justify-end pb-[80px]">
-        {messages.map((message, index) => (
+      <div className="flex flex-1 flex-col justify-end pb-[80px]">
+        {messages.map(message => (
           <ChatRow
+            key={message.id}
             chatMessageId={message.id}
             name={message.role === ChatRole.User ? session?.user?.name || "" : AI_NAME}
             message={message.content}
             type={message.role as ChatRole}
-            key={index}
             chatThreadId={chatThreadId}
             contentSafetyWarning={undefined}
-            feedback={undefined}
-            sentiment={undefined}
-            reason={undefined}
+            feedback={(message as any as ChatRowProps).feedback}
+            sentiment={(message as any as ChatRowProps).sentiment}
+            reason={(message as any as ChatRowProps).reason}
           />
         ))}
         {isLoading && <ChatLoading />}
