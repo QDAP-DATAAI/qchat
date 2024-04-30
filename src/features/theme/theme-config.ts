77d23ec7b425e4e794ff8b6e7d39f7ee0fcfc895
Keith Oak <keith.oak@chde.qld.gov.au>
2024-04-30 18:24:57 +1000

    Update APP_URL in theme-config.ts to include fallback value

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,13 +1,4 @@
 export const AI_NAME = "QChat"
 export const AI_TAGLINE = "The Queensland Government AI Assistant"
 export const AI_AUTHOR = "Queensland Government AI Unit"
-
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant."
-export const CHAT_DEFAULT_PERSONA = AI_NAME + " default"
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
-
-You have access to the following functions:
-1. create_img: You must only use the function create_img if the user asks you to create an image.`
-
-export const NEW_CHAT_NAME = "New chat"
 export const APP_URL = process.env.NEXTAUTH_URL_INTERNAL || "https://qchat.ai.qld.gov.au"
