9640eb3ea6983ace0362aead51ea06331c37279c
Keith Oak <keith.oak@chde.qld.gov.au>
2024-04-30 15:26:25 +1000

    Update NEXTAUTH_URL to NEXTAUTH_URL_INTERNAL in sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next"
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: process.env.NEXTAUTH_URL,
+      url: process.env.NEXTAUTH_URL_INTERNAL,
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
