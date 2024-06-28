17c039a136828dcc4c608783627c967637e93e7b
Fred <frederic.delage@chde.qld.gov.au>
2024-06-28 17:00:36 +1000

    feat: Add useOnScreen hook for detecting and  element visibility & useOnClickOutside for configuring modal's behaviour

--- src/components/announcement/agree-terms-and-conditions.tsx
+++ src/components/announcement/agree-terms-and-conditions.tsx
@@ -1,9 +1,10 @@
 import { Loader } from "lucide-react"
 import { useSession } from "next-auth/react"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useRef } from "react"
 
 import { APP_VERSION } from "@/app-global"
 
+import useOnScreen from "@/components/hooks/use-on-screen"
 import { Markdown } from "@/components/markdown/markdown"
 import Typography from "@/components/typography"
 import { showError, showSuccess } from "@/features/globals/global-message-store"
@@ -18,8 +19,11 @@ type AgreeTermsAndConditionProps = {
 export default function AgreeTermsAndConditions({ onClose }: AgreeTermsAndConditionProps): JSX.Element {
   const { update } = useSession()
   const [content, setContent] = useState<string>("Loading terms and conditions...")
-  const [loading, setLoading] = useState<boolean>(true)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+  const [canSubmit, setCanSubmit] = useState<boolean>(false)
+  const endOfScrollRef = useRef<HTMLDivElement>(null)
+  const isEndOfScroll = useOnScreen(endOfScrollRef)
 
   const handleSubmit = async (): Promise<void> => {
     try {
@@ -48,8 +52,12 @@ export default function AgreeTermsAndConditions({ onClose }: AgreeTermsAndCondit
       .then(async response => await response.text())
       .then(data => setContent(data))
       .catch(() => setContent("Failed to load terms and conditions, please try again later."))
-      .finally(() => setLoading(false))
+      .finally(() => setIsLoading(false))
   }, [])
+
+  useEffect(() => {
+    if (isEndOfScroll) setCanSubmit(true)
+  }, [isEndOfScroll])
 
   return (
     <Dialog>
@@ -57,12 +65,13 @@ export default function AgreeTermsAndConditions({ onClose }: AgreeTermsAndCondit
       <DialogContent>
         <div className="prose prose-slate max-w-4xl break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0">
           <Typography variant="h3">App Version {APP_VERSION}</Typography>
-          {loading ? "Loading terms and conditions..." : <Markdown content={content} />}
+          {isLoading ? "Loading terms and conditions..." : <Markdown content={content} />}
+          <div ref={endOfScrollRef} id="sentinel" />
         </div>
       </DialogContent>
       <DialogFooter>
         {isSubmitting && <Loader className={cn("animate-spin")} size={24} />}
-        <Button variant="default" onClick={handleSubmit} disabled={isSubmitting}>
+        <Button variant="default" onClick={handleSubmit} disabled={!canSubmit || isLoading || isSubmitting}>
           Agree with terms and conditions
         </Button>
       </DialogFooter>
--- src/features/ui/dialog.tsx
+++ src/features/ui/dialog.tsx
@@ -1,11 +1,14 @@
 "use client"
 
 import { X } from "lucide-react"
-import { ReactNode, createContext, forwardRef, useContext } from "react"
+import { ReactNode, createContext, forwardRef, useContext, useRef } from "react"
 
+import useOnClickOutside from "@/components/hooks/use-on-click-outside"
 import { cn } from "@/lib/utils"
 
-type DialogContextDefinition = unknown
+type DialogContextDefinition = {
+  onClose?: () => void
+}
 const DialogContext = createContext<DialogContextDefinition>({})
 export const useDialogContext = (): DialogContextDefinition => {
   const context = useContext(DialogContext)
@@ -13,35 +16,42 @@ export const useDialogContext = (): DialogContextDefinition => {
   return context
 }
 
-export const Dialog = forwardRef<HTMLDivElement, { children: ReactNode | ReactNode; className?: string }>(
-  ({ children, className }, ref) => (
-    <DialogContext.Provider value={{}}>
-      <div
-        ref={ref}
-        className={cn(
-          "my-[8rem] flex max-h-[calc(100vh-16rem)] min-h-[16rem] w-[500px] flex-col rounded-md border-2 bg-background",
-          className
-        )}
-      >
-        {children}
-      </div>
-    </DialogContext.Provider>
-  )
-)
-Dialog.displayName = "Dialog"
-
-export const DialogHeader = forwardRef<
+export const Dialog = forwardRef<
   HTMLDivElement,
   { children: ReactNode | ReactNode; className?: string; onClose?: () => void }
 >(({ children, className, onClose }, ref) => {
-  useDialogContext()
+  const modalRef = useRef<HTMLDivElement>(null)
+  useOnClickOutside(modalRef, () => onClose?.())
+
   return (
-    <div ref={ref} className={cn("relative rounded-t-md bg-background p-4 text-lg font-bold lg:p-6", className)}>
-      {onClose && <X className="absolute right-4 top-4 cursor-pointer" onClick={onClose} />}
-      {children}
-    </div>
+    <DialogContext.Provider value={{ onClose: onClose }}>
+      <div ref={modalRef} className={cn("my-[8rem] w-[500px]", className)}>
+        <div
+          ref={ref}
+          className={cn(
+            "flex max-h-[calc(100vh-16rem)] min-h-[16rem] flex-col rounded-md border-2 bg-background",
+            className
+          )}
+        >
+          {children}
+        </div>
+      </div>
+    </DialogContext.Provider>
   )
 })
+Dialog.displayName = "Dialog"
+
+export const DialogHeader = forwardRef<HTMLDivElement, { children: ReactNode | ReactNode; className?: string }>(
+  ({ children, className }, ref) => {
+    const { onClose } = useDialogContext()
+    return (
+      <div ref={ref} className={cn("relative rounded-t-md bg-background p-4 text-lg font-bold lg:p-6", className)}>
+        {onClose && <X className="absolute right-4 top-4 cursor-pointer" onClick={onClose} />}
+        {children}
+      </div>
+    )
+  }
+)
 DialogHeader.displayName = "DialogHeader"
 
 export const DialogContent = forwardRef<HTMLDivElement, { children: ReactNode | ReactNode; className?: string }>(
